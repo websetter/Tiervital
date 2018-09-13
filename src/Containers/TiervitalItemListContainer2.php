87a69c4998162345e6d9513186f536edac152631
werk38 <info@werk38.de>
2018-09-13 11:03:19 +0200

    Test Con 10
    test

--- src/Containers/TiervitalItemListContainer2.php
+++ src/Containers/TiervitalItemListContainer2.php
@@ -2,42 +2,12 @@
 
 namespace Tiervital\Containers;
 
-use Ceres\Config\CeresConfig;
-use IO\Services\ItemSearch\SearchPresets\TagItems;
-use IO\Services\ItemSearch\Services\ItemSearchService;
 use Plenty\Plugin\Templates\Twig;
 
 class TiervitalItemListContainer2
 {
     public function call(Twig $twig, $arg):string
     {
-        $tagList = [];
-
-        /** @var CeresConfig $ceresConfig */
-        $ceresConfig = pluginApp(CeresConfig::class);
-
-        $listType = $ceresConfig->itemLists->list2Type;
-
-        if($listType == 'tag_list')
-        {
-            /** @var ItemSearchService $itemSearchService */
-            $itemSearchService = pluginApp( ItemSearchService::class );
-
-            $itemSearchOptions = [
-                'tagIds' => explode(',', $ceresConfig->itemLists->list2TagIds),
-                'sorting' => $ceresConfig->itemLists->tagSorting
-            ];
-
-            $result = $itemSearchService->getResults([
-                                                         'tagItems' => TagItems::getSearchFactory( $itemSearchOptions )
-                                                     ]);
-
-            if(count($result['tagItems']))
-            {
-                $tagList = $result['tagItems']['documents'];
-            }
-        }
-
-        return $twig->render('Tiervital::Containers.ItemLists.ItemList2', ["item" => $arg[0], "itemList" => $tagList]);
+        return $twig->render('Tiervital::Containers.ItemLists.ItemList2', ["item" => $arg[0]]);
     }
 }
